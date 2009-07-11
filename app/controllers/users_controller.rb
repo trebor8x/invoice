ea50204681bde1d73ada75340dc556d51f845fb9
Tobias Schmidt <tobi@gronom.de>
2009-07-11 19:04:29 +0200

    add new restful_authentication plugin as submodule

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,25 @@
 class UsersController < ApplicationController
-
+  
   # render new.rhtml
   def new
+    @user = User.new
   end
-
+  
   def create
-    cookies.delete :auth_token
-    # protects against session fixation attacks, wreaks havoc with 
-    # request forgery protection.
-    # uncomment at your own risk
-    # reset_session
+    logout_keeping_session!
     @user = User.new(params[:user])
-    @user.save
-    if @user.errors.empty?
-      self.current_user = @user
+    success = @user && @user.save
+    if success && @user.errors.empty?
+            # Protects against session fixation attacks, causes request forgery
+      # protection if visitor resubmits an earlier form using back
+      # button. Uncomment if you understand the tradeoffs.
+      # reset session
+      self.current_user = @user # !! now logged in
       redirect_back_or_default('/')
       flash[:notice] = "Thanks for signing up!"
     else
+      flash[:error]  = "We couldn't set up that account, sorry. Please try again, or contact an admin (link is above)."
       render :action => 'new'
     end
   end
-
 end
