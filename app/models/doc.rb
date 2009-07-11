2ac0effef7cf2b4d09d40d4f877f3227f3f41a90
Tobias Schmidt <tobi@gronom.de>
2009-07-12 00:45:51 +0800

    add some nicer data representations
    Signed-off-by: trebor8x <trobbe@online.de>

--- app/models/doc.rb
+++ app/models/doc.rb
@@ -1,8 +1,8 @@
 class Doc < StoredFile
-	set_table_name "stored_files"
-	has_attachment :content_type => 'application/msword',
-	:max_size => 3.megabytes,
-	:storage => :file_system
+  set_table_name "stored_files"
+  has_attachment  :content_type => 'application/msword',
+                  :max_size => 3.megabytes,
+                  :storage => :file_system
 
-	validates_as_attachment
+  validates_as_attachment
 end
