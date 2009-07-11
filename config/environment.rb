cc6b39054f9b9c84cbc2f70200a4f2ccb0242930
Tobias Schmidt <tobi@gronom.de>
2009-07-12 00:43:03 +0800

    add german locale
    Signed-off-by: trebor8x <trobbe@online.de>

--- config/environment.rb
+++ config/environment.rb
@@ -43,6 +43,8 @@ Rails::Initializer.run do |config|
   # Run "rake -D time" for a list of tasks for finding time zone names. Comment line to use default local time.
   config.time_zone = 'UTC'
 
+  config.i18n.default_locale = :de
+  
   # Your secret key for verifying cookie session data integrity.
   # If you change this key, all old sessions will become invalid!
   # Make sure the secret is at least 30 characters and all random, 
