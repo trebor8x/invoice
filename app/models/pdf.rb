7f9e05130fafb510013e32bff27191f880de4956
Tobias Schmidt <tobi@gronom.de>
2009-07-12 20:48:37 +0800

    restrict access to files
    Signed-off-by: trebor8x <trobbe@online.de>

--- app/models/pdf.rb
+++ app/models/pdf.rb
@@ -1,7 +1,8 @@
 class Pdf < StoredFile
   has_attachment  :content_type => 'application/pdf',
                   :max_size => 3.megabytes,
-                  :storage => :s3
+                  :storage => :s3,
+                  :s3_access => :private
 
   validates_as_attachment
 end
