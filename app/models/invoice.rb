a9a6eee6b64206cd61607c9013eae38793aa867d
Tobias Schmidt <tobi@gronom.de>
2009-07-12 12:45:44 +0200

    validate associated pdf and doc

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,21 +1,23 @@
 class Invoice < ActiveRecord::Base
-	belongs_to :customer
-	has_one :pdf, :dependent => :destroy
-	has_one :doc, :dependent => :destroy
+  belongs_to :customer
+  has_one :pdf, :dependent => :destroy
+  has_one :doc, :dependent => :destroy
 
-	validates_presence_of :invoice_num
-	validates_numericality_of :invoice_num
-	validates_uniqueness_of :invoice_num
-	validates_presence_of :customer
+  validates_presence_of :invoice_num
+  validates_numericality_of :invoice_num
+  validates_uniqueness_of :invoice_num
+  validates_presence_of :customer
+  validates_associated :pdf, :if => :pdf
+  validates_associated :doc, :if => :doc
 
-	named_scope :ordered, :order => 'invoice_num'
+  named_scope :ordered, :order => 'invoice_num'
 
-	def self.find_invoices
-		find(:all, :order => "invoice_num")
-	end
+  def self.find_invoices
+    find(:all, :order => "invoice_num")
+  end
 
-	def self.new_invoice_num
-		(Invoice.maximum(:invoice_num) || 0) + 1
-	end
-	
+  def self.new_invoice_num
+    (Invoice.maximum(:invoice_num) || 0) + 1
+  end
+
 end
