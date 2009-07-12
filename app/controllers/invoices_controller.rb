3c1cd0e9d26e89fc160429919e205d24642957e7
Tobias Schmidt <tobi@gronom.de>
2009-07-12 18:38:48 +0800

    add attachment_fu as git submodule and fix problem with new invoices without pdf and doc files
    Signed-off-by: trebor8x <trobbe@online.de>

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,71 +1,74 @@
 class InvoicesController < ApplicationController
-	before_filter :login_required
-	def index
-		@invoices = Invoice.ordered
-	end
+  before_filter :login_required
+  def index
+    @invoices = Invoice.ordered
+  end
 
-	def show
-		@invoice = Invoice.find(params[:id])
-	end
+  def show
+    @invoice = Invoice.find(params[:id])
+  end
 
-	def new
-		@invoice = Invoice.new(:invoice_num => Invoice.new_invoice_num)
-		@invoice.pdf = Pdf.new
-		@invoice.doc = Doc.new
-	end
+  def new
+    @invoice = Invoice.new(:invoice_num => Invoice.new_invoice_num)
+    @invoice.pdf = Pdf.new
+    @invoice.doc = Doc.new
+  end
 
-	def edit
-		@invoice = Invoice.find(params[:id])
-	end
+  def edit
+    @invoice = Invoice.find(params[:id])
+  end
 
-	def create
-		@invoice = Invoice.new(params[:invoice])
-		@invoice.pdf = Pdf.new(params[:pdf])
-		@invoice.doc = Doc.new(params[:doc])
+  def create
+    @invoice = Invoice.new(params[:invoice])
+    if params[:pdf] && params[:pdf]["uploaded_data"] != ""
+      @invoice.pdf = Pdf.new(params[:pdf])
+    end
+    if params[:doc] && params[:doc]["uploaded_data"] != ""
+      @invoice.doc = Doc.new(params[:doc])
+    end
 
-		respond_to do |format|
-			if @invoice.save
-				flash[:notice] = 'Rechnung erfolgreich gespeichert.'
-				format.html { redirect_to(@invoice) }
-				format.xml  { render :xml => @invoice, :status => :created, :location => @invoice }
-			else
-				format.html { render :action => "new" }
-				format.xml  { render :xml => @invoice.errors, :status => :unprocessable_entity }
-			end
-		end
-	end
-	
-	def update
-		@invoice = Invoice.find(params[:id])
-		@pdf = Pdf.new(params[:pdf])
-		@doc = Doc.new(params[:doc])
-		if params[:pdf] && params[:pdf]["uploaded_data"] != ""
-			@invoice.pdf = @pdf
-		end
-		if params[:doc] && params[:doc]["uploaded_data"] != ""
-			@invoice.doc = @doc
-		end
-		respond_to do |format|
-			if @invoice.update_attributes(params[:invoice])
-				flash[:notice] = 'Rechnung erfolgreich aktualisiert.'
-				format.html { redirect_to(@invoice) }
-				format.xml  { head :ok }
-			else
-				format.html { render :action => "edit" }
-				format.xml  { render :xml => @invoice.errors, :status => :unprocessable_entity }
-			end
-		end
-	end
+    respond_to do |format|
+      if @invoice.save
+        flash[:notice] = 'Rechnung erfolgreich gespeichert.'
+        format.html { redirect_to(@invoice) }
+        format.xml  { render :xml => @invoice, :status => :created, :location => @invoice }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @invoice.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
 
-	def destroy
-		@invoice = Invoice.find(params[:id])
-		@invoice.destroy
+  def update
+    @invoice = Invoice.find(params[:id])
+    if params[:pdf] && params[:pdf]["uploaded_data"] != ""
+      @invoice.pdf = Pdf.new(params[:pdf])
+    end
+    if params[:doc] && params[:doc]["uploaded_data"] != ""
+      @invoice.doc = Doc.new(params[:doc])
+    end
+    
+    respond_to do |format|
+      if @invoice.update_attributes(params[:invoice])
+        flash[:notice] = 'Rechnung erfolgreich aktualisiert.'
+        format.html { redirect_to(@invoice) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @invoice.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
 
-		respond_to do |format|
-			format.html { redirect_to(invoices_url) }
-			format.xml  { head :ok }
-		end
-	end
+  def destroy
+    @invoice = Invoice.find(params[:id])
+    @invoice.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(invoices_url) }
+      format.xml  { head :ok }
+    end
+  end
 
 
 end
